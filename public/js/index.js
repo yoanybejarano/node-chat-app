6adfb012d56f1875f7ee5e245446643b5efdf433
yoanybejarano <yoanybejarano@gmail.com>
2018-09-16 18:43:21 -0500

    Emmit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'yeili',
-        text: 'Hey, this is Yoany.',
-        createdAt: new Date()
-    });
-
 });
 
 socket.on('disconnect', function () {
