fa27ab6c9b001cc5142551a22908e1f8c87d6408
yoanybejarano <yoanybejarano@gmail.com>
2018-09-18 22:54:08 -0500

    Scroll to bottom if user is close to bottom

--- server/server.js
+++ server/server.js
@@ -3,22 +3,21 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-var {generateMessage, generateLocationMessage} = require('./utils/message');
-
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage("Admin", "Welcome to the chat app"));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', generateMessage("Admin", "New user joined"));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
@@ -33,11 +32,8 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
-
 });
 
 server.listen(port, () => {
-    console.log('Server is up on port: ' + port);
+    console.log(`Server is up on ${port}`);
 });
-
-
